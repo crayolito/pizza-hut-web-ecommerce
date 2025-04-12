568880cfc481d583578831f4666250ac5e832ffe
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-04-12 09:49:43 -0400

    hola mundo

---
+++ assets/auxiliares-global.js
@@ -0,0 +1,8 @@
+class AuxiliaresGlobal {
+    static agregarCarrito(){
+        const contenedorPadre = document.querySelector('.h-ic-cantidad');
+        const mensaje = document.querySelector('.hicc-mensaje');
+    }
+    static limpiarCarrito(){}
+    static eliminarCarrito(){}
+}
