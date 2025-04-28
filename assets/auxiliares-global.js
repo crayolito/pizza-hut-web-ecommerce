e37ea528a300d6edc55267d9bd74a591dedab901
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-04-28 00:39:30 -0400

    hola mundo

--- assets/auxiliares-global.js
+++ assets/auxiliares-global.js
@@ -2307,8 +2307,9 @@ class PageCheckoutPH extends HTMLElement {
             </div>
           </div>
           `
-          var seraVisto = data.opcionesPrincipales.length == 0 && data.opcionesSecundarias.length == 0;
-
+          var seraVisto = (!data.opcionesPrincipales || data.opcionesPrincipales.length === 0) && 
+          (!data.opcionesSecundarias || data.opcionesSecundarias.length === 0);
+          
           contenidoHTML += `
           <div 
           data-seravisto="${seraVisto}"
