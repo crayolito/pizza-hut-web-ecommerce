0af5d63fb95bc410f2ac10877bc82f75dc84f104
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-04-28 11:40:41 -0400

    hola mundo

--- assets/auxiliares-global.js
+++ assets/auxiliares-global.js
@@ -3186,7 +3186,7 @@ class PageCheckoutPH extends HTMLElement {
       }
     };
     const myTest = 'shpat_' + '45f4a7476152f4881d058f87ce063698';
-    fetch(`https://${shopDomain}/admin/api/2023-07/orders.json`, {
+    fetch(this.urlConsulta, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
