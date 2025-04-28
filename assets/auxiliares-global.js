76d1bb87a8a686390117515c6397fabff0869a5d
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-04-28 11:35:59 -0400

    hola mundo

--- assets/auxiliares-global.js
+++ assets/auxiliares-global.js
@@ -3180,7 +3180,7 @@ class PageCheckoutPH extends HTMLElement {
         financial_status: "pending", // o "paid" si ya se ha pagado
         fulfillment_status: null,
         // Aquí guardamos el JSON grande como string en la nota del pedido
-        note: JSON.stringify(datosGrandesJSON),
+        note: JSON.stringify(informacionPedido),
         send_receipt: true,
         send_fulfillment_receipt: false
       }
