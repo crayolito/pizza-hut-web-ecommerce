0889f46a2be7f6a10633e52bc8599711a89b0f05
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-04-29 10:51:21 -0400

    hola mundo

--- assets/page-producto.js
+++ assets/page-producto.js
@@ -1334,6 +1334,7 @@ class PizzaHutProducto extends HTMLElement {
         console.log('Testeo de ver si tengo todo el carrito: ', this.carritoShopify);
         this.carritoShopify.informacionCompleta.items.forEach((item) => {
             const dataProducto = JSON.parse(item.properties.estructura);
+            console.log('Data producto: ', dataProducto);
             precioTotalCarrito += parseInt(dataProducto.producto.precioTotalConjunto);
             console.log('Precio total carrito: ', precioTotalCarrito);
         });
