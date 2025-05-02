c578f4e31a2a500853590bd37d1a9f1264f4f36a
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-05-01 23:44:30 -0400

    hola mundo

--- assets/page-iniciar-sesion.js
+++ assets/page-iniciar-sesion.js
@@ -55,6 +55,7 @@ class InicioSesion extends HTMLElement {
     this.btnIniciarSesion.addEventListener('click', this.iniciarSesion.bind(this));
     // this.btnGoogle.addEventListener('click', this.iniciarSesionGoogle.bind(this));
     // this.btnFacebook.addEventListener('click', this.iniciarSesionFacebook.bind(this));
+
     document.addEventListener('click', (event) => {
       // Verificar si ambos contenedores están visibles
       if (this.containerGeneral.style.display === 'flex' && this.containerVerificarNumero.style.display === 'flex') {
